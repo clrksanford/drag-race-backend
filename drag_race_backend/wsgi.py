d67f6e430c416948e6cdd844ea5bcb4acb30aea5
cns2382 <csanord@austin.utexas.edu>
2018-10-12 16:49:58 -0500

    Stub out initial django project

---
+++ drag_race_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for drag_race_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drag_race_backend.settings")
+
+application = get_wsgi_application()
